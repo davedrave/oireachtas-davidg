3cff48e0ae398c5dd1ce2cd1c5b585f94eeaf176
David Geraghty <davedrave@gmail.com>
2024-04-25 18:51:43 +0100

    Add DataLoaderFactory for future code to be able to dynamically create DataLoaders based on path type

---
+++ OireachtasAPI/OireachtasAPI/Factories/DataLoaderFactory.cs
@@ -0,0 +1,38 @@
+﻿using OireachtasAPI.DataLoaders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OireachtasAPI.Factories
+{
+    /// <summary>
+    /// Factory class for creating instances of IDataLoader.
+    /// </summary>
+    public class DataLoaderFactory
+    {
+        /// <summary>
+        /// Gets a data loader based on the specified source.
+        /// </summary>
+        /// <param name="source">The source URL or file path for the data loader.</param>
+        /// <returns>An <see cref="IDataLoader"/> that can load data from the specified source.</returns>
+        /// <exception cref="ArgumentException">Thrown if the source is null or empty.</exception>
+        public static IDataLoader GetLoader(string source)
+        {      
+            switch (source)
+            {
+                case string s when s.StartsWith("http", StringComparison.OrdinalIgnoreCase):
+                    return new HttpDataLoader();
+
+                case string s when s.EndsWith(".json", StringComparison.OrdinalIgnoreCase):
+                    return new FileDataLoader();
+                case null:
+                    throw new ArgumentNullException(nameof(source), "Source cannot be null.");
+                // Additional cases could be easily added here
+                default:
+                    throw new ArgumentException("Unsupported source type.", nameof(source));
+            }
+        }
+    }
+}
---
+++ OireachtasAPI/TestOireachtasAPI/FactoryTests/DataLoaderFactoryTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OireachtasAPI.DataLoaders;
+using OireachtasAPI.Factories;
+using System;
+
+namespace TestOireachtasAPI.FactoryTests
+{
+    /// <summary>
+    /// Tests for the DataLoaderFactory class.
+    /// </summary>
+    [TestClass]
+    public class DataLoaderFactoryTests
+    {
+        /// <summary>
+        /// Verifies that when provided with an HTTP URL, the DataLoaderFactory correctly returns an instance of <see cref="HttpDataLoader"/>.
+        /// </summary>
+        [TestMethod]
+        public void GetLoader_WithHttpUrl_ReturnsHttpDataLoader()
+        {
+            // Arrange
+            string url = "https://api.somewebsite.ie/v1/members?limit=50";
+
+            // Act
+            var loader = DataLoaderFactory.GetLoader(url);
+
+            // Assert
+            Assert.IsInstanceOfType(loader, typeof(HttpDataLoader));
+        }
+
+        /// <summary>
+        /// Verifies that when provided with a file path, the DataLoaderFactory returns an instance of <see cref="FileDataLoader"/>.
+        /// </summary>
+        [TestMethod]
+        public void GetLoader_WithFilePath_ReturnsFileDataLoader()
+        {
+            // Arrange
+            string filePath = "path/to/local/file.json";
+
+            // Act
+            var loader = DataLoaderFactory.GetLoader(filePath);
+
+            // Assert
+            Assert.IsInstanceOfType(loader, typeof(FileDataLoader));
+        }
+
+        /// <summary>
+        /// Checks whether an ArgumentNullException is thrown when a null argument is passed to the GetLoader method of DataLoaderFactory.
+        /// </summary>
+        [TestMethod]
+        public void GetLoader_WithNull_ThrowsArgumentException()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => DataLoaderFactory.GetLoader(null));
+        }
+
+        /// <summary>
+        /// Checks whether an ArgumentException is thrown when an empty argument is passed to the GetLoader method of DataLoaderFactory.
+        /// </summary>
+        [TestMethod]
+        public void GetLoader_WithEmptyString_ThrowsArgumentException()
+        {
+            Assert.ThrowsException<ArgumentException>(() => DataLoaderFactory.GetLoader(string.Empty));
+        }
+    }
+}
