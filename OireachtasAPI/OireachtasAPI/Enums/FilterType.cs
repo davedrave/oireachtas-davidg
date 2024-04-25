d2b05d3348c4669477764992d93b5e520644014a
David Geraghty <davedrave@gmail.com>
2024-04-25 22:19:26 +0100

    Include Enum to represent filter types as it will be referred to multiple times

---
+++ OireachtasAPI/OireachtasAPI/Enums/FilterType.cs
@@ -0,0 +1,11 @@
+﻿namespace OireachtasAPI.Enums
+{
+    /// <summary>
+    /// Represents the different types of bill filtering methods available.
+    /// </summary>
+    public enum FilterType
+    {
+        FilterBillsSponsoredBy,
+        FilterBillsLastUpdated
+    }
+}
